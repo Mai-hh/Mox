ed09b8309bbdacb876390c0238aa134cbda39f9b
莫淮昊 <83955130+Mai-hh@users.noreply.github.com>
2023-04-07 02:14:01 +0800

    init Scanner.kt

--- src/com/maihao/mox/Scanner.kt
+++ src/com/maihao/mox/Scanner.kt
@@ -1,6 +1,8 @@
 package com.maihao.mox
 
 import com.maihao.mox.TokenType.*
+
+
 class Scanner(
     private val source: String
 ) {
@@ -8,6 +10,13 @@ class Scanner(
     private var start = 0
     private var current = 0
     private var line = 1
+
+    companion object {
+        private val keywords = mapOf(
+            "and" to AND,
+            "class" to CLASS
+        )
+    }
 
     fun scanTokens(): List<Token> {
         while (!isAtEnd()) {
@@ -20,7 +29,8 @@ class Scanner(
     }
 
     private fun scanToken() {
-        when (advance()) {
+        val c = advance()
+        when (c) {
             '(' -> addToken(LEFT_PAREN)
             ')' -> addToken(RIGHT_PAREN)
             '{' -> addToken(LEFT_BRACE)
@@ -31,9 +41,91 @@ class Scanner(
             '+' -> addToken(PLUS)
             ';' -> addToken(SEMICOLON)
             '*' -> addToken(STAR)
-            else -> Mox.error(line, "Unexpected character.")
+            '!' -> addToken(if (match('=')) BANG_EQUAL else BANG)
+            '=' -> addToken(if (match('=')) EQUAL_EQUAL else EQUAL)
+            '<' -> addToken(if (match('=')) LESS_EQUAL else LESS)
+            '>' -> addToken(if (match('=')) GREATER_EQUAL else GREATER)
+            '/' -> {
+                if (match('/')) {
+                    // A comment goes until the end of the line.
+                    while (peek() != '\n' && !isAtEnd()) advance()
+                } else {
+                    addToken(SLASH)
+                }
+            }
+            ' ',
+            '\r',
+            '\t' -> { }
+            '\n' -> line++
 
+            '"' -> string()
+            else -> {
+                if (isDigit(c)) {
+                    number()
+                } else if(isAlpha(c)) {
+                    identifier()
+                } else {
+                    Mox.error(line, "Unexpected character.")
+                }
+            }
         }
+    }
+
+    private fun identifier() {
+        while (isAlphaNumeric(peek())) advance()
+        addToken(IDENTIFIER)
+    }
+
+    private fun isAlpha(c: Char): Boolean {
+        return c in 'a'..'z' || c in 'A'..'Z' || c == '_'
+    }
+
+    private fun isAlphaNumeric(c: Char): Boolean {
+        return isAlpha(c) || isDigit(c)
+    }
+
+    private fun isDigit(c: Char) = c in '0'..'9'
+
+    private fun string() {
+        while (peek() != '"' && !isAtEnd()) {
+            if (peek() == '\n') line++
+            advance()
+        }
+
+        if (isAtEnd()) {
+            Mox.error(line, "Unterminated string.")
+            return
+        }
+
+        // The closing ".
+        advance()
+
+        // Trim the surrounding quotes.
+        val value = source.substring(start + 1, current - 1)
+        addToken(STRING, value)
+    }
+
+    private fun number() {
+        while (isDigit(peek())) advance()
+
+        // Look for a fractional part.
+        if (peek() == '.' && isDigit(peekNext())) {
+            // Consume the "."
+            advance()
+
+            while (isDigit(peek())) advance()
+        }
+
+        addToken(NUMBER, source.substring(start, current).toDouble())
+    }
+
+    private fun peekNext(): Char {
+        return if (current + 1 >= source.length) Char.MIN_VALUE else source[current + 1]
+    }
+
+    private fun peek(): Char {
+        if (isAtEnd()) return Char.MIN_VALUE
+        return source[current]
     }
 
     private fun isAtEnd(): Boolean {
@@ -45,6 +137,14 @@ class Scanner(
     private fun addToken(type: TokenType, literal: Any? = null) {
         val text = source.substring(start, current)
         tokens.add(Token(type, text, literal, line))
+    }
+
+    private fun match(expected: Char): Boolean {
+        if (isAtEnd()) return false
+        if (source[current] != expected) return false
+
+        current++
+        return true
     }
 
 }
