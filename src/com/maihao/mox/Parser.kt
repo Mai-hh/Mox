a5e25bb9ffeefbf89e224c9c6c6f6b34214a7041
莫淮昊 <83955130+Mai-hh@users.noreply.github.com>
2023-04-12 17:13:07 +0800

    Create Parser.kt

---
+++ src/com/maihao/mox/Parser.kt
@@ -0,0 +1,126 @@
+package com.maihao.mox
+
+import com.maihao.mox.TokenType.*
+
+/*
+Parser rules:
+
+expression     → equality ;
+equality       → comparison ( ( "!=" | "==" ) comparison )* ;
+comparison     → term ( ( ">" | ">=" | "<" | "<=" ) term )* ;
+term           → factor ( ( "-" | "+" ) factor )* ;
+factor         → unary ( ( "/" | "*" ) unary )* ;
+unary          → ( "!" | "-" ) unary
+               | primary ;
+primary        → NUMBER | STRING | "true" | "false" | "nil"
+               | "(" expression ")" ;
+ */
+
+class Parser(val tokens: List<Token>) {
+    private var current = 0
+
+    private fun expression() = equality()
+
+    private fun equality(): Expr {
+        var expr = comparsion()
+
+        while (match(BANG_EQUAL, EQUAL_EQUAL)) {
+            val operator = previous()
+            val right = comparison()
+            expr = Expr.Binary(expr, operator, right)
+        }
+        return expr
+    }
+
+    private fun comparison(): Expr {
+        var expr = term()
+
+        while (match(GREATER, GREATER_EQUAL, LESS, LESS_EQUAL)) {
+            val operator = previous()
+            val right = term()
+            expr = Expr.Binary(expr, operator, right)
+        }
+
+        return expr
+    }
+
+    private fun term(): Expr {
+        var expr = factor()
+
+        while (match(MINUS, PLUS)) {
+            val operator = previous()
+            val right = factor()
+            expr = Expr.Binary(expr, operator, right)
+        }
+
+        return expr
+    }
+
+    private fun factor(): Expr {
+        var expr = unary()
+
+        while (match(SLASH, STAR)) {
+            val operator = previous()
+            val right = unary()
+            expr = Expr.Binary(expr, operator, right)
+        }
+
+        return expr
+    }
+
+    private fun unary(): Expr {
+        if (match(BANG, MINUS)) {
+            val operator = previous()
+            val right = unary()
+            return Expr.Unary(operator, right)
+        }
+        return primary()
+    }
+
+    private fun primary(): Expr {
+        if (match(FALSE)) return Expr.Literal(false)
+        if (match(TRUE)) return Expr.Literal(true)
+        if (match((NIL))) return Expr.Literal(null)
+        if (match(NUMBER, STRING)) return Expr.Literal(previous().literal)
+        if (match(LEFT_PAREN)) {
+            val expr = expression()
+            consume(RIGHT_PAREN, "Expect ')' after expression.")
+            return Expr.Grouping(expr)
+        }
+    }
+
+    private fun isAtEnd(): Boolean {
+        return peek().type == EOF
+    }
+
+    private fun peek(): Token {
+        return tokens[current]
+    }
+
+    private fun previous(): Token {
+        return tokens[current - 1]
+    }
+
+    private fun advance(): Token {
+        if (!isAtEnd()) current++
+        return previous()
+    }
+
+    private fun check(type: TokenType): Boolean {
+        return if (isAtEnd()) {
+            false
+        } else {
+            peek().type == type
+        }
+    }
+
+    private fun match(vararg types: TokenType): Boolean {
+        types.forEach { type ->
+            if (check(type)) {
+                advance()
+                return true
+            }
+        }
+        return false
+    }
+}
