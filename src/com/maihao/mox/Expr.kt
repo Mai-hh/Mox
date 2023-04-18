ad63313486b7f901957d8e1723d24bafd7bc23bb
莫淮昊 <83955130+Mai-hh@users.noreply.github.com>
2023-04-18 15:52:38 +0800

    Update Expr.kt

--- src/com/maihao/mox/Expr.kt
+++ src/com/maihao/mox/Expr.kt
@@ -1,48 +1,50 @@
 package com.maihao.mox
 
 sealed class Expr {
-	interface Visitor<R> {
-		fun visitBinaryExpr(expr: Binary): R
-		fun visitGroupingExpr(expr: Grouping): R
-		fun visitLiteralExpr(expr: Literal): R
-		fun visitUnaryExpr(expr: Unary): R
-		fun visitTernaryExpr(expr: Ternary): R
-	}
-	class Binary(val left: Expr,val operator: Token,val right: Expr,) : Expr() {
+    interface Visitor<R> {
+        fun visitBinaryExpr(expr: Binary): R
+        fun visitGroupingExpr(expr: Grouping): R
+        fun visitLiteralExpr(expr: Literal): R
+        fun visitUnaryExpr(expr: Unary): R
+        fun visitTernaryExpr(expr: Ternary): R
+    }
 
-		override fun <R> accept(visitor: Visitor<R>): R {
-			return visitor.visitBinaryExpr(this)
-		}
-	}
+    class Binary(val left: Expr, val operator: Token, val right: Expr) : Expr() {
 
-	class Grouping(val expression: Expr,) : Expr() {
+        override fun <R> accept(visitor: Visitor<R>): R {
+            return visitor.visitBinaryExpr(this)
+        }
+    }
 
-		override fun <R> accept(visitor: Visitor<R>): R {
-			return visitor.visitGroupingExpr(this)
-		}
-	}
+    class Grouping(val expression: Expr) : Expr() {
 
-	class Literal(val value: Any?,) : Expr() {
+        override fun <R> accept(visitor: Visitor<R>): R {
+            return visitor.visitGroupingExpr(this)
+        }
+    }
 
-		override fun <R> accept(visitor: Visitor<R>): R {
-			return visitor.visitLiteralExpr(this)
-		}
-	}
+    class Literal(val value: Any?) : Expr() {
 
-	class Unary(val operator: Token,val right: Expr,) : Expr() {
+        override fun <R> accept(visitor: Visitor<R>): R {
+            return visitor.visitLiteralExpr(this)
+        }
+    }
 
-		override fun <R> accept(visitor: Visitor<R>): R {
-			return visitor.visitUnaryExpr(this)
-		}
-	}
+    class Unary(val operator: Token, val right: Expr) : Expr() {
 
-	class Ternary(val first: Expr,val operator1: Token,val second: Expr,val operator2: Token,val third: Expr,) : Expr() {
+        override fun <R> accept(visitor: Visitor<R>): R {
+            return visitor.visitUnaryExpr(this)
+        }
+    }
 
-		override fun <R> accept(visitor: Visitor<R>): R {
-			return visitor.visitTernaryExpr(this)
-		}
-	}
+    class Ternary(val first: Expr, val operator1: Token, val second: Expr, val operator2: Token, val third: Expr) :
+        Expr() {
+
+        override fun <R> accept(visitor: Visitor<R>): R {
+            return visitor.visitTernaryExpr(this)
+        }
+    }
 
 
-	abstract fun <R> accept(visitor: Visitor<R>): R
+    abstract fun <R> accept(visitor: Visitor<R>): R
 }
