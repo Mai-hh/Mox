84ca1b27a302e5af00f7f5d2a0da744be4fa2001
MO HUAIHAO <mohuaihaoxm@gmail.com>
2024-11-02 01:28:11 -0400

    Resolving and Binding

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.8.21"
+    kotlin("jvm") version "2.0.21"
     application
 }
 
---
+++ src/main/kotlin/com/maihao/mox/Resolver.kt
@@ -0,0 +1,198 @@
+package com.maihao.mox
+
+import java.util.Stack
+
+private enum class FunctionType {
+    NONE,
+    FUNCTION
+}
+
+class Resolver(private val interpreter: Interpreter) : Expr.Visitor<Unit>, Stmt.Visitor<Unit> {
+    private val scopes = Stack<MutableMap<String, Boolean>>()
+    private var currentFunction = FunctionType.NONE
+
+    override fun visitAssignExpr(expr: Expr.Assign) {
+        resolve(expr.value)
+        resolveLocal(expr, expr.name)
+    }
+
+    override fun visitBinaryExpr(expr: Expr.Binary) {
+        resolve(expr.left)
+        resolve(expr.right)
+    }
+
+    override fun visitCallExpr(expr: Expr.Call) {
+        resolve(expr.callee)
+
+        for (argument in expr.arguments) {
+            resolve(argument)
+        }
+    }
+
+    override fun visitGroupingExpr(expr: Expr.Grouping) {
+        resolve(expr.expression)
+    }
+
+    override fun visitLambdaExpr(expr: Expr.Lambda) {
+
+    }
+
+    override fun visitLiteralExpr(expr: Expr.Literal) {
+
+    }
+
+    override fun visitVariableExpr(expr: Expr.Variable) {
+        if (scopes.isNotEmpty() && scopes.peek()[expr.name.lexeme] == false) {
+            Mox.error(expr.name, "Can't read local variable in its own initializer.")
+        }
+
+        resolveLocal(expr, expr.name)
+    }
+
+    override fun visitLogicalExpr(expr: Expr.Logical) {
+        resolve(expr.left)
+        resolve(expr.right)
+    }
+
+    override fun visitUnaryExpr(expr: Expr.Unary) {
+        resolve(expr.right)
+    }
+
+    override fun visitTernaryExpr(expr: Expr.Ternary) {
+        resolve(expr.first)
+        resolve(expr.second)
+        resolve(expr.third)
+    }
+
+    override fun visitBlockStmt(stmt: Stmt.Block) {
+        beginScope()
+        resolve(stmt.statements)
+        endScope()
+    }
+
+    override fun visitExpressionStmt(stmt: Stmt.Expression) {
+        resolve(stmt.expression)
+    }
+
+    override fun visitFunctionStmt(stmt: Stmt.Function) {
+        declare(stmt.name)
+        define(stmt.name)
+
+        resolveFunction(stmt, FunctionType.FUNCTION)
+    }
+
+    override fun visitLambdaStmt(stmt: Stmt.Lambda) {
+        beginScope()
+        for (param in stmt.params) {
+            declare(param)
+            define(param)
+        }
+        resolve(stmt.body)
+        endScope()
+    }
+
+    override fun visitIfStmt(stmt: Stmt.If) {
+        resolve(stmt.condition)
+        resolve(stmt.thenBranch)
+        if (stmt.elseBranch != null) resolve(stmt.elseBranch)
+    }
+
+    override fun visitPrintStmt(stmt: Stmt.Print) {
+        resolve(stmt.expression)
+    }
+
+    override fun visitReturnStmt(stmt: Stmt.Return) {
+        if (currentFunction == FunctionType.NONE) {
+            Mox.error(
+                token = stmt.keyword,
+                message = "Can't return from top-level code."
+            )
+        }
+
+        if (stmt.value != null) {
+            resolve(stmt.value)
+        }
+    }
+
+    override fun visitVarStmt(stmt: Stmt.Var) {
+        declare(stmt.name)
+        if (stmt.initializer != null) {
+            resolve(stmt.initializer)
+        }
+        define(stmt.name)
+    }
+
+    override fun visitWhileStmt(stmt: Stmt.While) {
+        resolve(stmt.condition)
+        resolve(stmt.body)
+    }
+
+    private fun resolve(stmt: Stmt) {
+        stmt.accept(this)
+    }
+
+    fun resolve(statements: List<Stmt>) {
+        statements.forEach { statement ->
+            resolve(statement)
+        }
+    }
+
+    private fun resolve(expr: Expr) {
+        expr.accept(this)
+    }
+
+    private fun resolveLocal(expr: Expr, name: Token) {
+        for (i in scopes.size - 1 downTo 0) {
+            if (scopes[i].containsKey(name.lexeme)) {
+                interpreter.resolve(expr, scopes.size - 1 - i)
+                return
+            }
+        }
+    }
+
+    private fun beginScope() {
+        scopes.push(HashMap())
+    }
+
+    private fun endScope() {
+        scopes.pop()
+    }
+
+    private fun declare(name: Token) {
+        if (scopes.isEmpty()) return
+
+        val scope = scopes.peek()
+
+        if (scope.containsKey(name.lexeme)) {
+            Mox.error(
+                token = name,
+                message = "Already a variable with this name in this scope."
+            )
+        }
+
+        scope[name.lexeme] = false
+    }
+
+    private fun define(name: Token) {
+        if (scopes.isEmpty()) return
+        scopes.peek()[name.lexeme] = true
+    }
+
+    private fun resolveFunction(
+        function: Stmt.Function,
+        type: FunctionType
+    ) {
+        val enclosingFunction = currentFunction
+        currentFunction = type
+
+        beginScope()
+        for (param in function.params) {
+            declare(param)
+            define(param)
+        }
+        resolve(function.body)
+        endScope()
+        currentFunction = enclosingFunction
+    }
+
+}
