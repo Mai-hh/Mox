7eadf5916c7fca8950465cafae468a3bc83bcb73
MO HUAIHAO <mohuaihaoxm@gmail.com>
2024-12-31 17:22:08 -0500

    Gradle fix
    upgrade Gradle
    
    Add shadowJar task for Jar packaging

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.0.21"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
     application
 }
 
@@ -11,6 +12,7 @@ repositories {
 }
 
 dependencies {
+    implementation(kotlin("stdlib"))
     testImplementation(kotlin("test-junit5"))
 }
 
@@ -23,6 +25,17 @@ kotlin {
 }
 
 application {
-    mainClass.set("com.maihao.mox.Mox")
+    // 你的主函数入口类
+    mainClass.set("com.maihao.mox.MoxKt")
 }
 
+// 配置 Shadow 打包任务
+tasks.shadowJar {
+    // 去掉默认的 "-all" 后缀，输出更干净
+    archiveClassifier.set("")
+
+    // 设置清单文件里的 Main-Class
+    manifest {
+        attributes["Main-Class"] = application.mainClass.get()
+    }
+}
